eb36a275d2aa72ce359ecbbf32fde27fb3b04c34
CodyBrewer <brewer.cody.m@gmail.com>
2019-02-28 06:22:37 -0700

    added card content border removed troubleshooting borders

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,9 +4,11 @@ import './Card.css';
 function CardContent(){
     return(
         <div className="card-content">
-            <h3>Get Started with React</h3>
-            <p>React makes it painless to create interactivs UIs. Design simple views for each state in your application.</p>
+            <div className="content-container">
+                <h3>Get Started with React</h3>
+                <p>React makes it painless to create interactivs UIs. Design simple views for each state in your application.</p>
             <a id="react-site">reactjs.org</a>
+            </div>
         </div>
     )
 }
