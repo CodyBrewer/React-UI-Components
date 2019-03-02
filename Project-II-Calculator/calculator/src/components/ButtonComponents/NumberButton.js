5859cd426710b750d6bdae8e2213b909a74ff10a
CodyBrewer <brewer.cody.m@gmail.com>
2019-03-02 10:30:49 -0700

    removed numbers array that we weren't using

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Button.css';
 
-const numbers = [7,8,9,4,5,6,1,2,3]
-
 function NumberButton (props) {
     return(
             <button className={props.buttonStyle}>{props.text}</button>
