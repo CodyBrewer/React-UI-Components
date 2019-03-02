05db320da33b0edd3805ba9bc448288892ffff28
CodyBrewer <brewer.cody.m@gmail.com>
2019-03-02 10:30:21 -0700

    set up number buttons

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const numbers = [7,8,9,4,5,6,1,2,3]
+
+function NumberButton (props) {
+    return(
+            <button className={props.buttonStyle}>{props.text}</button>
+    )
+}
+
+export default NumberButton;
