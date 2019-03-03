5f6de0095659de3b2c64a4dcc8dc109fb4493bce
CodyBrewer <brewer.cody.m@gmail.com>
2019-03-02 21:03:08 -0700

    refactored to have keypad componet attempted stretch

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,41 +1,24 @@
 import React from 'react';
 import './App.css';
-import NumberButton from './components/ButtonComponents/NumberButton';
-import ActionButton from './components/ButtonComponents/ActionButton';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
-
+import Keypad from './components/Keypad';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      result: 0
+    };
+  }
+  
+
   render(){
   return (
     <div>
-      <div className="calculator-container">
-        <div className ="display-container">
-          <CalculatorDisplay />
-        </div>
-        <div className="clear-button-container">
-          <NumberButton buttonStyle = "clear-button" text = "clear" />
-          <ActionButton buttonStyle ="action-button" text ="÷" />
-        </div>
-        <div className ="number-button-container">
-          <NumberButton buttonStyle ="num-button" text= "7"/>
-          <NumberButton buttonStyle ="num-button" text= "8"/>
-          <NumberButton buttonStyle ="num-button" text= "9"/>
-          <ActionButton buttonStyle ="action-button" text ="X"/>  
-          <NumberButton buttonStyle ="num-button" text= "4"/>
-          <NumberButton buttonStyle ="num-button" text= "5"/>
-          <NumberButton buttonStyle ="num-button" text= "6"/>
-          <ActionButton buttonStyle ="action-button" text ="-"/>   
-          <NumberButton buttonStyle ="num-button" text= "1"/>
-          <NumberButton buttonStyle ="num-button" text= "2"/>
-          <NumberButton buttonStyle ="num-button" text= "3"/>
-          <ActionButton buttonStyle ="action-button" text ="+"/>     
-        </div>
-        <div className="zero-button-container">     
-          <NumberButton buttonStyle ="num-button" text ="0" />
-          <ActionButton buttonStyle = "action-button" text ="=" />        
-        </div>
-      </div>
+
+        <CalculatorDisplay />
+
+      <Keypad onClick={this.onClick}/>
     </div>
   );
   }   
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Display.css';
 
-const CalculatorDisplay = props =>  {
-    return(
-        <div className = "total">0</div>
-    )
+class CalculatorDisplay extends React.Component {
+    render(){
+        let {result} = this.props;
+        return(
+            <div className="display-container">
+                <div className = "total">   
+                   0
+                </div>
+            </div>
+        );
+    }   
 }
-
+// //  render() {
+//     let {result} = this.props;
+//     return (
+//         <div className="result">
+//             <p>{result}</p>
+// </div>
 export default CalculatorDisplay;
